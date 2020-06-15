e56fcf86a90f02d3686cf01bd28694f1551b2c9c
Daniel Wagner <wagner.daniel87@gmail.com>
2020-06-15 18:48:50 +0200

    add validation

--- demo-project/src/app.ts
+++ demo-project/src/app.ts
@@ -1,8 +1,39 @@
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+function validate(validatableInput: Validatable): boolean {
+    let isValid = true;
+
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (validatableInput.minLength != null && typeof validatableInput.value === "string") {
+        isValid = isValid && validatableInput.value.trim().length >= validatableInput.minLength;
+    }
+    if (validatableInput.maxLength != null && typeof validatableInput.value === "string") {
+        isValid = isValid && validatableInput.value.trim().length <= validatableInput.maxLength;
+    }
+    if (validatableInput.min != null && typeof validatableInput.value === "number") {
+        isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    if (validatableInput.max != null && typeof validatableInput.value === "number") {
+        isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+
+    return isValid;
+}
+
 class ProjectInput {
 
     private templateElement: HTMLTemplateElement;
     private hostElement: HTMLDivElement;
-    private element: HTMLFormElement;
+    private formElement: HTMLFormElement;
     private titleInputElement: HTMLInputElement;
     private descriptionInputElement: HTMLInputElement;
     private peopleInputElement: HTMLInputElement;
@@ -12,28 +43,48 @@ class ProjectInput {
         this.hostElement = document.getElementById("app")! as HTMLDivElement;
 
         const importedNode = document.importNode(this.templateElement.content, true);
-        this.element = importedNode.firstElementChild as HTMLFormElement;
-        this.element.id = "user-input";
+        this.formElement = importedNode.firstElementChild as HTMLFormElement;
+        this.formElement.id = "user-input";
 
-        this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement;
-        this.descriptionInputElement = this.element.querySelector("#description") as HTMLInputElement;
-        this.peopleInputElement = this.element.querySelector("#people") as HTMLInputElement;
+        this.titleInputElement = this.formElement.querySelector("#title") as HTMLInputElement;
+        this.descriptionInputElement = this.formElement.querySelector("#description") as HTMLInputElement;
+        this.peopleInputElement = this.formElement.querySelector("#people") as HTMLInputElement;
 
         this.configure();
         this.attach();
     }
 
-    private attach() {
-        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    private configure() {
+        this.formElement.addEventListener("submit", this.submitHandler.bind(this));
     }
 
-    private configure() {
-        this.element.addEventListener("submit", this.submitHandler.bind(this));
+    private attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
     }
 
     private submitHandler(event: Event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+
+        if (
+            validate({ value: this.titleInputElement.value, required: true}) &&
+            validate({ value: this.descriptionInputElement.value, required: true, minLength: 5 }) &&
+            validate({ value: parseInt(this.peopleInputElement.value), required: true, min: 1, max: 5 })
+        ) {
+            console.log("Values are valid!");
+            const title = this.titleInputElement.value;
+            const description = this.descriptionInputElement.value;
+            const people = this.peopleInputElement.value;
+            this.clearInput();
+        }
+        else {
+            alert("Values are not valid");
+        }
+    }
+
+    private clearInput() {
+        this.titleInputElement.value = "";
+        this.descriptionInputElement.value = "";
+        this.peopleInputElement.value = "";
     }
 }
 
