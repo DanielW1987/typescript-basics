dc9e595e819278c480a052618dec382167066345
Daniel Wagner <wagner.daniel87@gmail.com>
2020-06-15 19:12:13 +0200

    add ProjectList

--- demo-project/src/app.ts
+++ demo-project/src/app.ts
@@ -88,4 +88,34 @@ class ProjectInput {
     }
 }
 
+class ProjectList {
+
+    private templateElement: HTMLTemplateElement;
+    private hostElement: HTMLDivElement;
+    private sectionElement: HTMLElement;
+
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById("project-list")! as HTMLTemplateElement;
+        this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.sectionElement = importedNode.firstElementChild as HTMLElement;
+        this.sectionElement.id = `${this.type}-projects`;
+
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderContent() {
+        this.sectionElement.querySelector("ul")!.id = `${this.type}-projects-list`;
+        this.sectionElement.querySelector("h2")!.textContent = `${this.type.toUpperCase()} PROJECTS`;
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement("beforeend", this.sectionElement);
+    }
+}
+
 const projectInput = new ProjectInput();
+const activeProjectList = new ProjectList("active");
+const finishedProjectList = new ProjectList("finished");
